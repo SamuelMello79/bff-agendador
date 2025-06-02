464a7f24495d0e3c272e05d4bef5491b9b094bf6
SamuelMello79 <mello.developer797@gmail.com>
2025-06-01 21:12:31 -0300

    [FEAT] Implementado o Cron, para enviar as notificações de tarefas de 5 em 5 minutos.

--- src/main/java/com/devmello/bff_agendador/infrastructure/client/NotificacaoClient.java
+++ src/main/java/com/devmello/bff_agendador/infrastructure/client/NotificacaoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "tarefas", url = "${api.notificacao.url}")
+@FeignClient(name = "notificacao", url = "${api.notificacao.url}")
 public interface NotificacaoClient {
     @PostMapping
     void sendEmail(@RequestBody TarefaDTOResponse tarefaDTO);
