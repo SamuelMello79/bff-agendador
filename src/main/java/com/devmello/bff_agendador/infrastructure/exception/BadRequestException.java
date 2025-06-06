83ddfa2452d156d29dcd9e9e06ced3d993087669
SamuelMello79 <mello.developer797@gmail.com>
2025-06-06 20:54:25 -0300

    [FEAT] Adicionado endpoint para buscar CEP

---
+++ src/main/java/com/devmello/bff_agendador/infrastructure/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.devmello.bff_agendador.infrastructure.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
